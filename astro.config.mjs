547b41ef3e2f94bc3db75997b45048958f26ec40
Hamunii <samuel.suni04@gmail.com>
2026-06-18 23:21:38 +0300

    clarify stuff in ienumerator hooking article

--- astro.config.mjs
+++ astro.config.mjs
@@ -80,5 +80,7 @@ export default defineConfig({
 	devToolbar: { enabled: false },
 	// Whenever a page changes, add it to the redirects.
 	// https://docs.astro.build/en/guides/routing/#redirects
-	redirects: {},
+	redirects: {
+		"/hooking/ienumerators/": "/hooking/iterators/"
+	},
 });
